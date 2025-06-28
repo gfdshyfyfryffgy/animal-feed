095b54ed8048d81eb8853771de71a0727b525e7f
gfdshyfyfryffgy <sochinnovative19@gmail.com>
2025-06-28 18:35:37 +0530

    Fix: resolved peer dependency conflicts

--- src/Pages/ContactSection.jsx
+++ src/Pages/ContactSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser"; 
+
 import horse from "../assets/horse.jpg";
 
 const COLORS = {
