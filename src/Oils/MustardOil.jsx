e63609b5fdbcf9eefe8d466495fd25ba341d7734
gfdshyfyfryffgy <sochinnovative19@gmail.com>
2025-09-16 18:38:24 +0530

    some content update adding some icons and images

--- src/Oils/MustardOil.jsx
+++ src/Oils/MustardOil.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import m1 from "../assets/products/MustardOil.png";
+import m1 from "../assets/products/mustardoil.png";
 
 const COLORS = {
   heading: "#3E3F5B",
